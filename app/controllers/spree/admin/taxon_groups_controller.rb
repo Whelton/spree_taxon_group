3b2be5f927d47c9584949bb4c84f845777826ba0
James Whelton <james@whelton.io>
2015-10-18 19:44:32 +0400

    sort taxons

--- app/controllers/spree/admin/taxon_groups_controller.rb
+++ app/controllers/spree/admin/taxon_groups_controller.rb
@@ -3,6 +3,11 @@ module Spree
     class TaxonGroupsController < ResourceController
       before_action :load_taxons, only: [:new, :edit]
 
+      def edit
+        # Sort taxons based on if taxon is in taxon group's taxons
+        @taxons = @taxons.sort_by{|taxon| taxon.id.in?(@taxon_group.taxon_ids) ? 0 : 1}
+      end
+
       private
 
       def load_taxons
